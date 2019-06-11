af106712b7de6d2e23c9eebb2543b019e827a4ca
john871031 <john8710310121@gmail.com>
2019-06-12 01:08:48 +0800

    Initial commit

---
+++ app/src/main/java/com/example/fadacai/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.fadacai;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button b1 =(Button)findViewById(R.id.button1);
+        b1.setOnClickListener(new Button.OnClickListener(){
+        @Override
+            public void onClick(View v){
+            Intent intent = new Intent();
+            intent.setClass(MainActivity.this, ADD.class);
+            startActivity(intent);
+        }
+        });
+    }
+}
